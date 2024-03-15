a2c0c09e0303ed148350c19d375d0550c7cd3148
Iuri <iuri.ti@chiaperini.com.br>
2024-03-15 16:00:29 -0300

    Commit

--- coin_see/views.py
+++ coin_see/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+import requests
 
 def index(request):
-    return render(request, 'coin_see/index.html')
+    url = 'https://api.coingecko.com/api/v3/coins/markets'
+    params = {
+        'vs_currency': 'brl',
+        'order': 'market_cap_desc',
+        
+        'per_page': 5,
+        'page': 1
+    }
+
+
+
+    response = requests.get(url, params=params)
+    coins = response.json()
+
+    return render(request, 'coin_see/coins.html', {'coins': coins})
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,11 @@ module.exports = {
       backgroundImage: theme => ({
         'sea': "url('/static/img/background.jpg')",
       }),
+      fontFamily: {
+        'baloo-bhai': ['"Baloo Bhai 2"', 'cursive'],
+        'baloo-bhaina': ['"Baloo Bhaina 2"', 'cursive'],
+        'baloo-thambi': ['"Baloo Thambi 2"', 'cursive'],
+      },
       
     },
   },
